4faff2bfbfdcac22e85c06f9dfbc60d6ebfd8336
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-09-03 15:35:02 -0400

    Added `magnitude` and `normalize` and supporting tests.

--- src/scintilla/numeric.clj
+++ src/scintilla/numeric.clj
@@ -25,20 +25,37 @@
   (+ [v1 v2])
   (- [v] [v1 v2])
   (* [v s])
-  (/ [v s]))
+  (/ [v s])
+  (magnitude [v])
+  (normalize [v]))
 
 (extend-type clojure.lang.PersistentVector
   Tuple
   (+ [[_ _ _ w1 :as v1] [_ _ _ w2 :as v2]]
-      (case [w1 w2]
-        ([1 0] [0 1] [0 0]) (mapv clojure.core/+ v1 v2)
-        (throw (Exception. "Cannot add two points."))))
+    (case [w1 w2]
+      ([1 0] [0 1] [0 0]) (mapv clojure.core/+ v1 v2)
+      (throw (Exception. "Cannot add two points."))))
   (- ([v] (mapv clojure.core/- v))
-     ([[_ _ _ w1 :as v1] [_ _ _ w2 :as v2]]
-      (case [w1 w2]
-        ([1 1] [1 0] [0 0]) (mapv clojure.core/- v1 v2)
-        (throw (Exception. "Cannot subtract a point from a vector.")))))
+   ([[_ _ _ w1 :as v1] [_ _ _ w2 :as v2]]
+    (case [w1 w2]
+      ([1 1] [1 0] [0 0]) (mapv clojure.core/- v1 v2)
+      (throw (Exception. "Cannot subtract a point from a vector.")))))
   (* [[x y z w] s]
-      [(clojure.core/* s x) (clojure.core/* s y) (clojure.core/* s z) w])
-  (/ [v s]
-     (* v (clojure.core// 1.0 s))))
+    (if (= w 0)
+      [(clojure.core/* s x) (clojure.core/* s y) (clojure.core/* s z) w]
+      (throw (Exception. "Cannot scale a point."))))
+  (/ [[_ _ _ w :as v] s]
+    (if (= w 0)
+      (* v (clojure.core// 1.0 s))
+      (throw (Exception. "Cannot scale a point."))))
+  (magnitude [[x y z w]]
+    (if (= w 0)
+      (->> [x y z]
+           (map #(Math/pow % 2))
+           (reduce clojure.core/+ 0)
+           (Math/sqrt))
+      (throw (Exception. "Cannot scale a point."))))
+  (normalize [[_ _ _ w :as v]]
+    (if (= w 0)
+      (/ v (magnitude v))
+      (throw (Exception. "Cannot normalize a point.")))))
