8ddbc9f9b82ccc43cc121614ee9c70e33d85106e
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-09-22 16:14:25 -0400

    Changed implementation of canvas so that its dimensions can be easily retrieved after initialization.

--- src/scintilla/canvas.clj
+++ src/scintilla/canvas.clj
@@ -2,14 +2,12 @@
 
 (defn make-canvas
   [x y]
-  (into {}
-    (for [i (range x) j (range y)]
-      [[i j] [0 0 0]])))
+  (vec (repeat y (vec (repeat x [0 0 0])))))
 
 (defn write-pixel
   [canvas x y color]
-  (assoc-in canvas [[x y]] color))
+  (assoc-in canvas [y x] color))
 
 (defn read-pixel
   [canvas x y]
-  (get-in canvas [[x y]]))
+  (get-in canvas [y x]))
