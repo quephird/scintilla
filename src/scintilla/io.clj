79a0695202675d544c7ad856db231c0606abf3da
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-09-22 21:44:21 -0400

    Added implementation for writing out PPM body.

--- src/scintilla/io.clj
+++ src/scintilla/io.clj
@@ -1,7 +1,28 @@
 (ns scintilla.io)
 
+(defn clamp-and-scale
+  [c]
+  (-> (cond (< c 0.0) 0.0 (> c 1.0) 1.0 :else c)
+      (* 255.0)
+      Math/round))
+
 (defn ppm-header
   [canvas]
   (let [height (count canvas)
         width  (count (first canvas))]
     (format "P3\n%s %s\n255" width height)))
+
+(defn ppm-color
+  [color]
+  (apply format "%s %s %s" (map clamp-and-scale color)))
+
+(defn ppm-body
+  [canvas]
+  (let [height (count canvas)
+        width  (count (first canvas))]
+    (->> (for [row canvas]
+           (->> (for [pixel row] (ppm-color pixel))
+                (interpose " ")
+                (apply str)))
+         (interpose "\n")
+         (apply str))))
--- test/scintilla/io_test.clj
+++ test/scintilla/io_test.clj
@@ -8,3 +8,15 @@
     (let [canvas (c/make-canvas 640 480)
           header (ppm-header canvas)]
       (is (= "P3\n640 480\n255" header)))))
+
+(deftest testing-ppm-body
+  (testing "body is written out properly"
+    (let [canvas         (-> (c/make-canvas 5 3)
+                             (c/write-pixel 0 0 [1.5 0 0])
+                             (c/write-pixel 2 1 [0 0.5 0])
+                             (c/write-pixel 4 2 [0 0 1]))
+          body           (ppm-body canvas)
+          expected-value (clojure.string/join "\n" ["255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+                                                    "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"
+                                                    "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"])]
+      (is (= body expected-value)))))
