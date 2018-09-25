4ad795fb93aad6916a9183bde2f33a483c08ec90
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-09-25 12:42:51 -0400

    Added implementation to produce PPM body.

--- src/scintilla/io.clj
+++ src/scintilla/io.clj
@@ -12,17 +12,32 @@
         width  (count (first canvas))]
     (format "P3\n%s %s\n255" width height)))
 
-(defn ppm-color
-  [color]
-  (apply format "%s %s %s" (map clamp-and-scale color)))
+(defn new-line-length
+  [acc & new-color-components]
+  (+ (count (last (clojure.string/split-lines acc)))
+     (count (clojure.string/join " " (map str new-color-components)))))
+
+(defn- format-row
+  [row]
+  (->> row
+       (reduce (fn [acc color]
+         (let [[r g b] (map clamp-and-scale color)]
+           (cond
+             (empty? acc)
+               (format "%s %s %s" r g b)
+             (< (new-line-length acc r g b) 70)
+               (format "%s %s %s %s" acc r g b)
+             (< (new-line-length acc r g) 70)
+               (format "%s %s %s\n%s" acc r g b)
+             (< (new-line-length acc r) 70)
+               (format "%s %s\n%s %s" acc r g b)
+             :else
+               (format "%s\n%s %s %s" acc r g b)))) "")))
 
 (defn ppm-body
   [canvas]
   (let [height (count canvas)
         width  (count (first canvas))]
-    (->> (for [row canvas]
-           (->> (for [pixel row] (ppm-color pixel))
-                (interpose " ")
-                (apply str)))
+    (->> (for [row canvas] (format-row row))
          (interpose "\n")
          (apply str))))
--- test/scintilla/io_test.clj
+++ test/scintilla/io_test.clj
@@ -19,4 +19,18 @@
           expected-value (clojure.string/join "\n" ["255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
                                                     "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"
                                                     "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"])]
-      (is (= body expected-value)))))
+      (is (= body expected-value))))
+
+  (testing "body has no lines longer than 70 characters"
+    (let [canvas         (reduce (fn [acc [x y]]
+                                   (c/write-pixel acc x y [1 0.8 0.6]))
+                                 (c/make-canvas 10 2)
+                                 (for [x (range 10) y (range 2)] [x y]))
+          body           (ppm-body canvas)
+          lines          ["255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+                          "153 255 204 153 255 204 153 255 204 153 255 204 153"
+                          "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+                          "153 255 204 153 255 204 153 255 204 153 255 204 153"]
+          expected-value (clojure.string/join "\n" lines)]
+      (is (= body expected-value))))
+      )
