0a9889ccde47ab057bc6d873f0a9e90fd631c22a
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-10-17 00:22:03 -0400

    Added implementation and tests for determinant.

--- src/scintilla/matrix.clj
+++ src/scintilla/matrix.clj
@@ -1,5 +1,11 @@
 (ns scintilla.matrix)
 
+(def I
+  [[1 0 0 0]
+   [0 1 0 0]
+   [0 0 1 0]
+   [0 0 0 1]])
+
 (defn transpose
   [m]
   (let [column-count (count (first m))]
@@ -8,9 +14,51 @@
                     (get row column-idx)) m)) (range column-count))))
 
 (defn matrix-times
-  [m1 m2]
+  [matrix1 matrix2]
   (mapv (fn [row1]
-          (mapv (fn [col1]
-                  (reduce + (map * row1 col1)))
-                (transpose m2)))
-        m1))
+          (mapv (fn [column2]
+                  (reduce + 0 (map * row1 column2)))
+                (transpose matrix2)))
+        matrix1))
+
+(defn tuple-times
+  [matrix tuple]
+  (mapv (fn [row]
+          (mapv (fn [element]
+                  (reduce + 0 (map * row tuple)))
+                tuple))
+        matrix))
+
+(defn scalar-times
+  [matrix scalar]
+  (mapv (fn [row]
+          (mapv (fn [cell] (* scalar cell)) row)) matrix))
+
+(defn remove-at-index
+  [index vector]
+  (into (subvec vector 0 index) (subvec vector (inc index) (count vector))))
+
+(defn submatrix
+  [row-idx column-idx matrix]
+  (->> matrix
+       (mapv (fn [row] (remove-at-index column-idx row)))
+       (remove-at-index row-idx)))
+
+(defn sign
+  [row-idx column-idx]
+  (if (even? (+ row-idx column-idx))
+    1
+    -1))
+
+(defn determinant
+  [matrix]
+  (case (count matrix)
+    2
+      (let [[[a b] [c d]] matrix]
+        (- (* a d) (* b c)))
+    (reduce (fn [acc column-idx]
+              (+ acc (* (sign 0 column-idx)
+                        (get (first matrix) column-idx)
+                        (determinant (submatrix 0 column-idx matrix)))))
+            0
+            (range (count matrix)))))
--- test/scintilla/matrix_test.clj
+++ test/scintilla/matrix_test.clj
@@ -2,6 +2,20 @@
   (:require [scintilla.matrix :refer :all]
             [clojure.test :refer :all]))
 
+(deftest testing-transpose
+  (testing "transposing a 4x4 matrix"
+    (let [a [[0 9 3 0]
+             [9 8 0 8]
+             [1 8 5 3]
+             [0 0 5 8]]
+          expected-value [[0 9 1 0]
+                          [9 8 8 0]
+                          [3 0 5 5]
+                          [0 8 3 8]]]
+     (is (= expected-value (transpose a)))))
+  (testing "transposing the identity matrix"
+     (is (= I (transpose I)))))
+
 (deftest testing-matrix-times
   (testing "multiplying two 4X4 matrices"
     (let [a [[1 2 3 4]
@@ -16,4 +30,63 @@
                           [31 64 128 256]
                           [38 79 158 316]
                           [45 94 188 376]]]
-      (is (= expected-value (matrix-times a b))))))
+      (is (= expected-value (matrix-times a b)))))
+
+(deftest testing-tuple-times
+  (testing "multiplying a 4X4 matrix with a 4 element tuple"
+    (let [a [[1 2 3 4]
+             [2 4 4 2]
+             [8 6 4 1]
+             [0 0 0 1]]
+          b [1 2 3 1]
+          expected-value [18 24 33 1]]
+      (is (= expected-value (tuple-times a b))))))
+
+(deftest testing-scalar-times
+  (testing "multiplying a 4X4 matrix with a scalar"
+    (let [a [[1 2 3 4]
+             [2 4 4 2]
+             [8 6 4 1]
+             [0 0 0 1]]
+          b 2
+          expected-value [[2 4 6 8]
+                          [4 8 8 4]
+                          [16 12 8 2]
+                          [0 0 0 2]]]
+      (is (= expected-value (scalar-times a b)))))))
+
+(deftest testing-submatrix
+  (testing "a 3X3 matrix"
+    (let [a [[1 5 0]
+             [-3 2 7]
+             [0 6 -3]]
+          expected-value [[-3 2]
+                          [0 6]]]
+      (is (= expected-value (submatrix 0 2 a)))))
+
+  (testing "a 3X3 matrix"
+    (let [a [[-6 1 1 6]
+             [-8 5 8 6]
+             [-1 0 8 2]
+             [-7 1 -1 1]]
+          expected-value [[-6 1 6]
+                          [-8 8 6]
+                          [-7 -1 1]]]
+      (is (= expected-value (submatrix 2 1 a))))))
+
+(deftest testing-determinant
+  (testing "a 2X2 matrix"
+    (let [a [[1 5]
+             [-3 2]]]
+      (is (= 17 (determinant a)))))
+  (testing "a 3X3 matrix"
+    (let [a [[1 2 6]
+             [-5 8 -4]
+             [2 6 4]]]
+      (is (= -196 (determinant a)))))
+  (testing "a 4X4 matrix"
+    (let [a [[-2 -8 3 5]
+             [-3 1 7 3]
+             [1 2 -9 6]
+             [-6 7 7 -9]]]
+      (is (= -4071 (determinant a))))))
