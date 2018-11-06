0a8df1f9a010b7d847f7dce3004851b6b6e75014
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-11-05 20:09:11 -0800

    Completed implementation for a sphere, accounting for transformation.

--- src/scintilla/shapes.clj
+++ src/scintilla/shapes.clj
@@ -1,8 +1,12 @@
 (ns scintilla.shapes
-  (:require [scintilla.matrix :refer [I₄]]
+  (:require [scintilla.matrix :refer :all]
             [scintilla.tuple :refer :all]))
 
+(def default-color [1 0 0])
+
 (defn make-sphere
+  ([]
+    (make-sphere default-color))
   ([color]
     (make-sphere color I₄))
   ([color matrix]
@@ -12,6 +16,16 @@
 
 (defmulti find-normal (fn [shape _] (:shape-type shape)))
 
+;; TODO: Look into how to better do this in a less hacky way.
 (defmethod find-normal :sphere
-  [shape point]
-  (normalize (- point [0.0 0.0 0.0 1.0])))
+  [{:keys [matrix]} world-point]
+  (let [object-point (-> matrix
+                         inverse
+                         (tuple-times world-point))
+        object-normal (- object-point [0.0 0.0 0.0 1.0])]
+    (-> matrix
+        inverse
+        transpose
+        (tuple-times object-normal)
+        (assoc 3 0)  ;; Per the book, this is a hack
+        normalize)))
--- test/scintilla/shapes_test.clj
+++ test/scintilla/shapes_test.clj
@@ -1,28 +1,40 @@
 (ns scintilla.shapes-test
   (:require [clojure.test :refer :all]
             [scintilla.numeric :refer :all]
-            [scintilla.shapes :refer :all]))
+            [scintilla.shapes :refer :all]
+            [scintilla.transformation :as t]))
 
 (deftest testing-find-normal
   (testing "the normal on a sphere at a point on the x axis"
-    (let [sphere (make-sphere [1 0 0])
+    (let [sphere (make-sphere)
           point  [1 0 0 1]
           expected-value [1 0 0 0]]
       (is (≈ expected-value (find-normal sphere point)))))
   (testing "the normal on a sphere at a point on the y axis"
-    (let [sphere (make-sphere [1 0 0])
+    (let [sphere (make-sphere)
           point  [0 1 0 1]
           expected-value [0 1 0 0]]
       (is (≈ expected-value (find-normal sphere point)))))
   (testing "the normal on a sphere at a point on the z axis"
-    (let [sphere (make-sphere [1 0 0])
+    (let [sphere (make-sphere)
           point  [0 0 1 1]
           expected-value [0 0 1 0]]
       (is (≈ expected-value (find-normal sphere point)))))
   (testing "the normal on a sphere at a non-axial point"
-    (let [sphere (make-sphere [1 0 0])
+    (let [sphere (make-sphere)
           √3⟋3   (/ (Math/sqrt 3) 3.0)
           point  [√3⟋3 √3⟋3 √3⟋3 1]
           expected-value [√3⟋3 √3⟋3 √3⟋3 0]]
       (is (≈ expected-value (find-normal sphere point)))))
-)
+  (testing "the normal on a translated sphere"
+    (let [transform (t/translation-matrix 0 1 0)
+          sphere (make-sphere [1 0 0] transform)
+          point  [0 1.70711 -0.70711 1]
+          expected-value [0 0.70711 -0.70711 0]]
+      (is (≈ expected-value (find-normal sphere point)))))
+  (testing "the normal on a scaled sphere"
+    (let [transform (t/scaling-matrix 1.0 0.5 1.0)
+          sphere (make-sphere [1 0 0] transform)
+          point  [0 0.70711 -0.70711 1]
+          expected-value [0 0.97014 -0.24254 0]]
+      (is (≈ expected-value (find-normal sphere point))))))
