bac716715e3d7f1379e3cc655a6a206a34252a84
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-11-09 00:46:56 -0500

    Added four tests.

--- test/scintilla/lighting_test.clj
+++ test/scintilla/lighting_test.clj
@@ -7,9 +7,60 @@
 (deftest testing-lighting
   (let [material a/default-material
         surface-position [0 0 0 1]]
-    (testing "lighting with the eye between the light and the surface"
+    (testing "lighting with the eye between the light and the surface
+
+                                |
+                                |
+                   🔆   👁  <----|
+                                |
+                                |
+      "
       (let [eye-direction [0 0 -1 0]
             surface-normal [0 0 -1 0]
             light (make-light [0 0 -10 1] [1 1 1])
             expected-value [1.9 1.9 1.9]]
-        (is (≈ expected-value (lighting material light surface-position eye-direction surface-normal)))))))
+        (is (≈ expected-value (lighting material light surface-position eye-direction surface-normal)))))
+    (testing "lighting with the eye between light and surface, eye offset 45°
+
+                           👁
+                             ⟍  |
+                               ⟍|
+                   🔆       <----|
+                                |
+                                |
+      "
+      (let [eye-direction [0 0.70711 -0.70711 0]
+            surface-normal [0 0 -1 0]
+            light (make-light [0 0 -10 1] [1 1 1])
+            expected-value [1.0 1.0 1.0]]
+        (is (≈ expected-value (lighting material light surface-position eye-direction surface-normal)))))
+    (testing "lighting with opposite surface, light offset 45°
+
+                           🔆
+                             ⟍  |
+                               ⟍|
+                   👁       <----|
+                                |
+                                |
+      "
+      (let [eye-direction [0 0 -1 0]
+            surface-normal [0 0 -1 0]
+            light (make-light [0 10 -10 1] [1 1 1])
+            expected-value [0.7364 0.7364 0.7364]]
+        (is (≈ expected-value (lighting material light surface-position eye-direction surface-normal)))))
+    (testing "lighting with eye in the path of the reflection vector
+
+                           🔆
+                             ⟍  |
+                               ⟍|
+                            <----|
+                               ⟋|
+                             ⟋  |
+                           👁
+      "
+      (let [eye-direction [0 -0.70711 -0.70711 0]
+            surface-normal [0 0 -1 0]
+            light (make-light [0 10 -10 1] [1 1 1])
+            expected-value [1.63722 1.63722 1.63722]]
+        (is (≈ expected-value (lighting material light surface-position eye-direction surface-normal)))))
+        ))
