42b1f755c8358b3fbc7656f3d6249d99d388095a
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-11-20 15:00:30 -0500

    Implemented `ray-for` and added supporting tests; also moved computation of half-world-width and half-world-height into 'constructor' for camera.

--- src/scintilla/camera.clj
+++ src/scintilla/camera.clj
@@ -1,17 +1,48 @@
-(ns scintilla.camera)
+(ns scintilla.camera
+  (:require [scintilla.matrix :refer [I₄] :as m]
+            [scintilla.ray :as r]
+            [scintilla.tuple :as u]))
 
 (defn make-camera
-  [width height field-of-view]
-  {:width width
-   :height height
-   :field-of-view field-of-view})
+  "Constructs a camera defined by the width and height of its canvas
+   as well as its field of view, which is the solid angle limiting
+   the camera's perspective. The distance between the eye and the canvas
+   is 1.0 world unit."
+  ([pixel-width pixel-height field-of-view]
+    (make-camera pixel-width pixel-height field-of-view I₄))
+  ([pixel-width pixel-height field-of-view transform]
+    (let [half-view    (Math/tan (/ field-of-view 2.0))
+          aspect-ratio (/ pixel-width pixel-height)
+          ;; Note that we associate these two properties with the camera itself
+          ;; to avoid recomputing them every time we call `pixel-size-for`
+          ;; and/or `ray-for`
+          [half-world-width half-world-height] (if (>= aspect-ratio 1)
+                                                 [half-view (/ half-view aspect-ratio)]
+                                                 [(* half-view aspect-ratio) half-view])]
+  {:pixel-width       pixel-width
+   :half-world-width  half-world-width
+   :pixel-height      pixel-height
+   :half-world-height half-world-height
+   :field-of-view     field-of-view
+   :transform         transform})))
 
 (defn pixel-size-for
-  "Computes the size of a pixel in world units for the given camera."
-  [{:keys [width height field-of-view] :as camera}]
-  (let [half-view  (Math/tan (/ field-of-view 2.0))
-        aspect     (/ height width)
-        [half-width half-height] (if (>= aspect 1)
-                                     [half-view (/ half-view aspect)]
-                                     [(* half-view aspect) half-view])]
-    (/ (* half-width 2.0) height)))
+  "Computes the size of a pixel in world units for the given camera.
+   Note that pixels are always square so we only need to compute
+   the size of one dimension."
+  [{:keys [half-world-width pixel-width] :as camera}]
+  (/ (* half-world-width 2.0) pixel-width))
+
+(defn ray-for
+  "Computes the ray for the given camera and (x,y) coordinates of its canvas,
+   in terms of the coordinate system correspondent with the inverse
+   of the camera's transform matrix."
+  [{:keys [half-world-width half-world-height transform] :as camera} x y]
+  (let [pixel-size (pixel-size-for camera)
+        [offset-x offset-y] (map #(* (+ % 0.5) pixel-size) [x y])
+        [world-x world-y]   (map - [half-world-width half-world-height] [offset-x offset-y])
+        inverse-transform   (m/inverse transform)
+        point'              (m/tuple-times inverse-transform [world-x world-y -1 1])
+        origin'             (m/tuple-times inverse-transform [0 0 0 1])
+        direction'          (u/normalize (u/subtract point' origin'))]
+    (r/make-ray origin' direction')))
--- test/scintilla/camera_test.clj
+++ test/scintilla/camera_test.clj
@@ -1,7 +1,10 @@
 (ns scintilla.camera-test
   (:require [clojure.test :refer :all]
             [scintilla.camera :refer :all]
-            [scintilla.numeric :refer :all]))
+            [scintilla.matrix :as m]
+            [scintilla.numeric :refer :all]
+            [scintilla.ray :as r]
+            [scintilla.transformation :as t]))
 
 (deftest testing-pixel-size-for
   (testing "the pixel size for a landscape canvas"
@@ -10,3 +13,23 @@
   (testing "the pixel size for a portrait canvas"
     (let [camera (make-camera 125 200 π⟋2)]
       (is (≈ 0.01 (pixel-size-for camera))))))
+
+(deftest testing-ray-for
+  (testing "constructing a ray through the center of the canvas"
+    (let [camera (make-camera 201 101 π⟋2)
+          ray    (ray-for camera 100 50)]
+      (is (≈ [0 0 0 1] (:point ray)))
+      (is (≈ [0 0 -1 0] (:direction ray)))))
+  (testing "constructing a ray through the corner of the canvas"
+    (let [camera (make-camera 201 101 π⟋2)
+          ray    (ray-for camera 0 0)]
+      (is (≈ [0 0 0 1] (:point ray)))
+      (is (≈ [0.66519 0.33259 -0.66851] (:direction ray)))))
+  (testing "constructing a ray through the center of the canvas"
+    (let [R         (t/rotation-y-matrix π⟋4)
+          T         (t/translation-matrix 0 -2 5)
+          transform (m/matrix-times R T)
+          camera    (make-camera 201 101 π⟋2 transform)
+          ray       (ray-for camera 100 50)]
+      (is (≈ [0 2 -5 1] (:point ray)))
+      (is (≈ [0.70711 0 -0.70711 0] (:direction ray))))))
