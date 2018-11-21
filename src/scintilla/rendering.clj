e6fc159af41fbc99754c57db2c4a0bd27b32e264
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-11-20 23:54:05 -0500

    Reworked scintilla.rendering with new camera data structure and added supporting test.

--- src/scintilla/rendering.clj
+++ src/scintilla/rendering.clj
@@ -1,44 +1,21 @@
 (ns scintilla.rendering
-  (:require [scintilla.file :as f]
-            [scintilla.lighting :as l]
-            [scintilla.materials :as a]
-            [scintilla.ray :as r]
-            [scintilla.scene :as e]
-            [scintilla.shapes :as s]
-            [scintilla.transformation :as t]
-            [scintilla.tuple :as u]))
-
-;; TODO: Think of some tests for this namespace!!!
-;; TODO: Need to package wall and camera metrics into perspective box
-(defn pixel-color
-  "Computes the color of the pixel at the x,y coordinates
-   for the given scene and canvas"
-  [[x y]
-   {:keys [light] :as scene}
-   wall-center
-   wall-dimensions
-   canvas-dimensions
-   camera-point]
-  (let [wall-point (e/pixel->scene [x y] canvas-dimensions wall-center wall-dimensions)
-        direction (u/normalize (u/subtract wall-point camera-point))
-        ray (r/make-ray camera-point direction)]
-    (r/color-for scene ray)))
+  (:require [scintilla.camera :as c]
+            [scintilla.file :as f]
+            [scintilla.ray :as r]))
 
 (defn render
-  "Produces new canvas, with specified dimensions, with object scene rendered to it"
-  [scene [canvas-w canvas-h :as canvas-dimensions]]
-  (let [wall-dimensions [7.0 7.0]
-        camera-point [0.0 0.0 -5.0 1]
-        wall-center [0.0 0.0 10.0 1]]
-    ;; For each pixel in the canvas...
+  "Renders the scene using the camera and returns the image data in a canvas data structure."
+  [{:keys [pixel-width pixel-height] :as camera} scene]
+  (let [])
     (into []
-      (for [x (range canvas-w)]
+      (for [x (range pixel-width)]
         (into []
-          (for [y (range canvas-h)]
-            (pixel-color [x y] scene wall-center wall-dimensions canvas-dimensions camera-point)))))))
+          (for [y (range pixel-height)]
+            (r/color-for scene (c/ray-for camera x y)))))))
 
 (defn render-to-file
-  [scene canvas-dimensions filename]
-  (-> scene
-      (render canvas-dimensions)
+  "Renders the scene using the camera and saves the image to a file."
+  [camera scene filename]
+  (-> camera
+      (render scene)
       (f/save-as-ppm filename)))
---
+++ test/scintilla/rendering_test.clj
@@ -0,0 +1,27 @@
+(ns scintilla.rendering-test
+  (:require [clojure.test :refer :all]
+            [scintilla.camera :as c]
+            [scintilla.canvas :as v]
+            [scintilla.materials :as a]
+            [scintilla.matrix :refer [I₄]]
+            [scintilla.numeric :refer :all]
+            [scintilla.rendering :refer :all]
+            [scintilla.scene :as e]
+            [scintilla.shapes :as s]
+            [scintilla.transformation :as t]))
+
+(deftest testing-render
+  (testing "rendering a world with a camera"
+    (let [material1      (a/make-material [0.8 1.0 0.6] 0.1 0.7 0.2 200)
+          sphere1        (s/make-sphere [1.0 0.0 0.0] I₄ material1)
+          transform2     (t/scaling-matrix 0.5 0.5 0.5)
+          sphere2        (s/make-sphere [1.0 0.0 0.0] transform2 a/default-material)
+          scene          (e/add-objects (e/make-scene) [sphere1 sphere2])
+          from           [0 0 -5 1]
+          to             [0 0 0 1]
+          up             [0 1 0 0]
+          view-transform (t/view-transform-matrix-for from to up)
+          camera         (c/make-camera 11 11 π⟋2 view-transform)
+          canvas         (render camera scene)
+          expected-value [0.38066 0.47583 0.2855]]
+      (is (≈ expected-value (v/read-pixel canvas 5 5))))))
