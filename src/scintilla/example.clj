204a7f82fb386a02cfb943bcaa17789dc4b26606
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-11-21 22:06:24 -0500

    Updated first example to use new camera; added new example from book.

--- src/scintilla/example.clj
+++ src/scintilla/example.clj
@@ -1,7 +1,9 @@
 (ns scintilla.example
-  (:require [scintilla.lighting :as l]
+  (:require [scintilla.camera :as c]
+            [scintilla.lighting :as l]
             [scintilla.materials :as a]
             [scintilla.matrix :as m]
+            [scintilla.numeric :refer :all]
             [scintilla.rendering :as r]
             [scintilla.scene :as e]
             [scintilla.shapes :as s]
@@ -9,24 +11,87 @@
 
 (defn sphere-with-light
   []
-  (let [transform (t/scaling-matrix 0.5 0.5 0.5)
-        material (a/make-material [1 0.2 1] 0.1 0.9 0.9 20)
-        sphere  (s/make-sphere [0 1 0] transform material)
-        light (l/make-light [-10 10 -10 1] [1 1 1])
-        scene (e/make-scene [sphere] light)
-        canvas-dimensions [100 100]]
-    (r/render-to-file scene canvas-dimensions "sphere-with-light.ppm")))
+  (let [transform      (t/scaling-matrix 0.5 0.5 0.5)
+        material       (a/make-material [1 0.2 1] 0.1 0.9 0.9 20)
+        sphere         (s/make-sphere material transform)
+        light          (l/make-light [-10 10 -10 1] [1 1 1])
+        scene          (e/make-scene [sphere] light)
+        view-transform (t/view-transform-matrix-for [0 0 -1 1]
+                                                    [0 0 0 1]
+                                                    [0 1 0 0])
+        camera         (c/make-camera 100 100 π⟋2 view-transform)]
+    (r/render-to-file camera scene "sphere-with-light.ppm")))
 
-(defn four-spheres-with-light
+(defn- make-floor
   []
-  (let [S            (t/scaling-matrix 0.5 0.5 0.5)
-        coordinates  [[-1 1 0] [1 1 0] [1 -1 0] [-1 -1 0]]
-        translations (map #(apply t/translation-matrix %) coordinates)
-        transforms   (map #(m/matrix-times S %) translations)
-        colors       [[0.3 0 0.5] [0 0.3 0.7] [0.9 0.3 0.2] [1 0 0.5]]
-        materials    (map #(a/make-material % 0.1 0.9 0.9 20) colors)
-        spheres      (map #(s/make-sphere [0 1 0] %1 %2) transforms materials)
-        light        (l/make-light [-10 10 -10 1] [1 1 1])
-        scene        (e/make-scene spheres light)
-        canvas-dimensions [400 400]]
-    (r/render-to-file scene canvas-dimensions "four-spheres-with-light.ppm")))
+  (let [material  (-> a/default-material
+                      (a/set-color [1 0.9 0.9])
+                      (a/set-specular 0.0))
+        transform (t/scaling-matrix 10 0.01 10)]
+    (s/make-sphere material transform)))
+
+(defn- make-left-wall
+  []
+  (let [material  (-> a/default-material
+                      (a/set-color [1 0.9 0.9])
+                      (a/set-specular 0.0))
+        transform (->> (t/scaling-matrix 10 0.01 10)
+                       (m/matrix-times (t/rotation-x-matrix π⟋2))
+                       (m/matrix-times (t/rotation-y-matrix (- π⟋4)))
+                       (m/matrix-times (t/translation-matrix 0 0 5)))]
+    (s/make-sphere material transform)))
+
+(defn- make-right-wall
+  []
+  (let [material  (-> a/default-material
+                      (a/set-color [1 0.9 0.9])
+                      (a/set-specular 0.0))
+        transform (->> (t/scaling-matrix 10 0.01 10)
+                       (m/matrix-times (t/rotation-x-matrix π⟋2))
+                       (m/matrix-times (t/rotation-y-matrix π⟋4))
+                       (m/matrix-times (t/translation-matrix 0 0 5)))]
+    (s/make-sphere material transform)))
+
+(defn- make-left-sphere
+  []
+  (let [material  (-> a/default-material
+                      (a/set-color [1 0.8 0.1])
+                      (a/set-diffuse 0.7)
+                      (a/set-specular 0.3))
+        transform (-> (t/translation-matrix -1.5 0.33 -0.75)
+                      (m/matrix-times (t/scaling-matrix 0.3 0.3 0.3)))]
+    (s/make-sphere material transform)))
+
+(defn- make-middle-sphere
+  []
+  (let [material  (-> a/default-material
+                      (a/set-color [0.1 1.0 0.5])
+                      (a/set-diffuse 0.7)
+                      (a/set-specular 0.3))
+        transform (t/translation-matrix -0.5 1 0.5)]
+    (s/make-sphere material transform)))
+
+(defn- make-right-sphere
+  []
+  (let [material  (-> a/default-material
+                      (a/set-color [0.5 1 0.1])
+                      (a/set-diffuse 0.7)
+                      (a/set-specular 0.3))
+        transform (-> (t/translation-matrix 1.5 0.5 -0.5)
+                      (m/matrix-times (t/scaling-matrix 0.5 0.5 0.5)))]
+    (s/make-sphere material transform)))
+
+(defn three-spheres-in-corner
+  []
+  (let [floor          (make-floor)
+        left-wall      (make-left-wall)
+        right-wall     (make-right-wall)
+        left-sphere    (make-left-sphere)
+        middle-sphere  (make-middle-sphere)
+        right-sphere   (make-right-sphere)
+        scene          (e/make-scene [left-sphere middle-sphere right-sphere floor left-wall right-wall] l/default-light)
+        view-transform (t/view-transform-matrix-for [0 1.5 -5 1]
+                                                    [0 1 0 1]
+                                                    [0 1 0 0])
+        camera         (c/make-camera 100 50 π⟋3 view-transform)]
+    (r/render-to-file camera scene "three-spheres-in-corner.ppm")))
