b6b3db441437accdc731306f2cda103d3b19a325
ⅅ₳ℕⅈⅇℒℒⅇ Ҝⅇℱℱʘℝⅅ <danielle_kefford@hotmail.com>
2018-11-23 10:18:15 -0500

    Removed superfluous let in scintilla.rendering/render.

--- src/scintilla/rendering.clj
+++ src/scintilla/rendering.clj
@@ -6,12 +6,11 @@
 (defn render
   "Renders the scene using the camera and returns the image data in a canvas data structure."
   [{:keys [pixel-width pixel-height] :as camera} scene]
-  (let [])
-    (into []
-      (for [y (range pixel-height)]
-        (into []
-          (for [x (range pixel-width)]
-            (r/color-for scene (c/ray-for camera x y)))))))
+  (into []
+    (for [y (range pixel-height)]
+      (into []
+        (for [x (range pixel-width)]
+          (r/color-for scene (c/ray-for camera x y)))))))
 
 (defn render-to-file
   "Renders the scene using the camera and saves the image to a file."
