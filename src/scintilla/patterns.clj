0d7685f9b12911ab7b009b51d8cc24f02c86f724
quephird <danielle_kefford@hotmail.com>
2018-12-05 02:19:03 -0500

    Added support for pattern transformations and correspondent tests.

--- src/scintilla/patterns.clj
+++ src/scintilla/patterns.clj
@@ -1,10 +1,14 @@
-(ns scintilla.patterns)
+(ns scintilla.patterns
+  (:require [scintilla.matrix :refer [I₄] :as m]))
 
 (defn make-stripe-pattern
-  [color-1 color-2]
-  {:pattern-type :stripe
-   :color-1      color-1
-   :color-2      color-2})
+  ([color-1 color-2]
+   (make-stripe-pattern color-1 color-2 I₄))
+  ([color-1 color-2 transform]
+   {:pattern-type :stripe
+    :transform    transform
+    :color-1      color-1
+    :color-2      color-2}))
 
 (defmulti color-for
   "Each implementation computes the raw color of the
@@ -16,7 +20,11 @@
 (defmethod color-for :stripe
   [{:keys [surface-point] :as prepared-hit}]
   (let [{:keys [color-1 color-2]} (get-in prepared-hit [:shape :material :pattern])
-        {:keys [surface-point]}   prepared-hit
-        [x _ _ _]                 surface-point
-        even-stripe?             (-> x (mod 2.0) int zero?)]
+        pattern-transform         (get-in prepared-hit [:shape :material :pattern :transform])
+        object-transform          (get-in prepared-hit [:shape :matrix])
+        pattern-space-point       (->> surface-point
+                                       (m/tuple-times (m/inverse object-transform))
+                                       (m/tuple-times (m/inverse pattern-transform)))
+        [x _ _ _]                 pattern-space-point
+        even-stripe?              (-> x (mod 2.0) int zero?)]
     (if even-stripe? color-1 color-2)))
