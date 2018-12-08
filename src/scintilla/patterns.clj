46dfc46fc55bf5ad707a411b8031725aa4349b44
quephird <danielle_kefford@hotmail.com>
2018-12-08 13:33:47 -0500

    Implemented checker pattern.

--- src/scintilla/patterns.clj
+++ src/scintilla/patterns.clj
@@ -20,6 +20,15 @@
     :color-1      color-1
     :color-2      color-2}))
 
+(defn make-checker-pattern
+  ([color-1 color-2]
+   (make-checker-pattern color-1 color-2 I₄))
+  ([color-1 color-2 transform]
+   {:pattern-type :checker
+    :transform    transform
+    :color-1      color-1
+    :color-2      color-2}))
+
 (defn make-gradient-pattern
   ([color-1 color-2]
    (make-gradient-pattern color-1 color-2 I₄))
@@ -45,7 +54,7 @@
                                        (m/tuple-times (m/inverse object-transform))
                                        (m/tuple-times (m/inverse pattern-transform)))
         [x _ _ _]                 pattern-space-point
-        even-stripe?              (-> x (mod 2.0) int zero?)]
+        even-stripe?              (-> x (mod 2.0) Math/floor zero?)]
     (if even-stripe? color-1 color-2)))
 
 (defmethod color-for :ring
@@ -60,9 +69,23 @@
         even-ring?                (-> (+ (* x x) (* z z))
                                       Math/sqrt
                                       (mod 2.0)
-                                      int
+                                      Math/floor
                                       zero?)]
     (if even-ring? color-1 color-2)))
+
+(defmethod color-for :checker
+  [{:keys [surface-point] :as prepared-hit}]
+  (let [{:keys [color-1 color-2]} (get-in prepared-hit [:shape :material :pattern])
+        pattern-transform         (get-in prepared-hit [:shape :material :pattern :transform])
+        object-transform          (get-in prepared-hit [:shape :matrix])
+        pattern-space-point       (->> surface-point
+                                       (m/tuple-times (m/inverse object-transform))
+                                       (m/tuple-times (m/inverse pattern-transform)))
+        [x y z _]                 pattern-space-point
+        even-square?              (-> (+ (Math/floor x) (Math/floor y) (Math/floor z))
+                                      (mod 2.0)
+                                      zero?)]
+    (if even-square? color-1 color-2)))
 
 (defmethod color-for :gradient
   [{:keys [surface-point] :as prepared-hit}]
@@ -73,4 +96,4 @@
                                        (m/tuple-times (m/inverse object-transform))
                                        (m/tuple-times (m/inverse pattern-transform)))
         [x _ _ _]                 pattern-space-point]
-    (c/add color-1 (c/scalar-times (c/subtract color-2 color-1) (- x (int x))))))
+    (c/add color-1 color-2 (c/scalar-times (c/subtract color-2 color-1) x))))
