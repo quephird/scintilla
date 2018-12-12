6a7f82504f7ce5a08adceb5be54eebd55f367220
quephird <danielle_kefford@hotmail.com>
2018-12-12 13:59:52 -0500

    Added new properties to material data structure.

--- src/scintilla/materials.clj
+++ src/scintilla/materials.clj
@@ -1,13 +1,15 @@
 (ns scintilla.materials)
 
 (def default-material
-  {:ambient 0.1
-   :color [1 1 1]
-   :diffuse 0.9
-   :pattern nil
-   :reflective 0.0
-   :shininess 200
-   :specular 0.9})
+  {:ambient          0.1
+   :color            [1 1 1]
+   :diffuse          0.9
+   :pattern          nil
+   :reflective       0.0
+   :refractive-index 1.0
+   :shininess        200
+   :specular         0.9
+   :transparency     0.0})
 
 (defn make-material
   [overrides]
