b044129d460795e5ffe16a8396abd0c9ac4d290a
quephird <danielle_kefford@hotmail.com>
2019-04-12 17:45:09 -0700

    Added docstrings and refactored to my current satisfaction.

--- src/scintilla/obj.clj
+++ src/scintilla/obj.clj
@@ -26,14 +26,20 @@
    :groups        {:default []}
    :current-group :default})
 
+(defn- statement-type-for
+  "Dispatcher for `parse-line` multimethod."
+  [line]
+  (-> line (str/split #" ") first))
+
 (defmulti parse-line
   "Takes a line read in from an OBJ file as well as
    the set of all objects parsed up until this point
    and returns the new set of objects if the line
-   can be parsed. This multimethod dispatches on the
-   first word on the line, representing an object type."
+   can be parsed, otherwise returns the parsed objects
+   untouched. This multimethod dispatches on the
+   first word on the line, representing a statement type."
   (fn [line _]
-    (-> line (str/split #" ") first)))
+    (statement-type-for line)))
 
 ;; Vertex statement
 (defmethod parse-line "v"
@@ -42,13 +48,42 @@
         point      (vec (map #(Double/parseDouble %) args))]
     (update-in parser-results [:vertices] conj point)))
 
+(defn- parse-all-indices
+  "Helper function to parse all indices from the vertex data in a
+   face statement. Vertex data can be in the following forms:
+
+     * f 1 2 3 4
+     * f 1//1 2//2 3//3 4//4
+     * f 1/4/1 2/3/2 3/2/3 4/1/4
+
+   ... and so this method breaks up a single string containing
+   _just_ the vertex data and returns a list of lists of
+   integers. Each sublist contains:
+
+     * at a minimum the index into the list of vertices as the first element
+     * an optional second element as the index into the list of vertex textures
+     * and optional third element as the index into the list of vertex normals"
+  [vertex-data]
+  (->> vertex-data
+       (map #(str/split % #"/"))
+       (map (fn [index-group]
+              (map #(Integer/parseInt %) index-group)))))
+
+(defn make-triangle-triplets-for
+  "Helper function to take a list of vertex indices for a single face
+   and returning a list of triplets of indices for each triagle in the
+   face."
+  [[starting-index & other-indices :as vertex-indices]]
+  (let [vertex-pairs (partition 2 1 other-indices)]
+    (mapv #(into [] (cons starting-index %)) vertex-pairs)))
+
 ;; Face statement
 (defmethod parse-line "f"
   [line parser-results]
-  (let [[_ & vertices] (str/split line #"\s+")
-        [starting-index & other-indices] (vec (map #(Integer/parseInt %) vertices))
-        vertex-pairs   (partition 2 1 other-indices)
-        new-triangles  (mapv #(into [] (cons starting-index %)) vertex-pairs)
+  (let [[_ & vertex-data] (str/split line #"\s+")
+        all-indices    (parse-all-indices vertex-data)
+        vertex-indices (map first all-indices)
+        new-triangles  (make-triangle-triplets-for vertex-indices)
         current-group  (:current-group parser-results)]
     (update-in parser-results [:groups current-group] into new-triangles)))
 
@@ -59,7 +94,7 @@
         new-group    (keyword name)]
     (assoc-in parser-results [:current-group] new-group)))
 
-;; All blank lines and other non-supported statements
+;; Handle for all blank lines and other non-supported statements
 (defmethod parse-line :default
   [_ parser-results]
   parser-results)
@@ -77,26 +112,36 @@
             new-results
             (recur new-results remaining)))))))
 
-(defn make-triangle-for
-  [triangle vertices]
-  (let [[p1 p2 p3] (->> triangle
+(defn- make-triangle-for
+  "Takes a triple of triangle indices, and the list of all vertices,
+   and returns a Scintilla triangle object."
+  [triangle-indices vertices]
+  (let [[p1 p2 p3] (->> triangle-indices
                         (map dec)
                         (map #(get vertices %))
                         (map #(conj % 1)))]
     (s/make-triangle p1 p2 p3)))
 
-(defn make-group-for
-  [group vertices]
-  (let [triangles (map #(make-triangle-for % vertices) group)]
+(defn- make-group-for
+  "Takes a set of parsed data for a group, named or default, and
+   the list of all vertices, and returns a Scintilla group object."
+  [group-data vertices]
+  (let [triangles (map #(make-triangle-for % vertices) group-data)]
     (g/make-group triangles)))
 
 (defn results->group
+  "Takes the complete set of parsed vertices, textures, groups, etc.
+   and returns a Scintilla group containing all objects represented
+   in the OBJ file."
   [{:keys [groups vertices] :as results}]
   (let [named-groups (map (fn [[_ indices]]
                             (make-group-for indices vertices)) groups)]
     (g/make-group named-groups)))
 
 (defn load-obj-file
+  "Intended to be the main public interface to this namespace, this
+   takes a filename for an OBJ file, parses it entirely, and returns
+   the Scintilla representation of the entire scene in the file."
   [filename]
   (-> filename
       parse-file
