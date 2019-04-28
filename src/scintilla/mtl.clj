bbaf35bbeb6e675d3582068fdf21628ada7f0ea7
quephird <danielle_kefford@hotmail.com>
2019-04-28 12:27:16 -0400

    First draft of parser for .mtl files.

---
+++ src/scintilla/mtl.clj
@@ -0,0 +1,70 @@
+(ns scintilla.mtl
+  (:require [scintilla.groups :as g]
+            [scintilla.shapes :as s]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn make-empty-parse-results
+  []
+  {:materials {}
+   :current-material nil})
+
+(defn- statement-type-for
+  "Dispatcher for `parse-line` multimethod."
+  [line]
+  (-> line (str/split #" ") first))
+
+(defmulti parse-line
+  "Takes a line read in from an MTL file as well as
+   the set of all materials parsed up until this point
+   and returns the new set of meterials if the line
+   can be parsed, otherwise returns the original parsed results
+   untouched. This multimethod dispatches on the
+   first word on the line, representing a statement type."
+  (fn [line _]
+    (statement-type-for line)))
+
+;; New material statement
+(defmethod parse-line "newmtl"
+  [line parser-results]
+  (let [[_ name & _] (str/split line #"\s+")
+        new-material (keyword name)]
+    (assoc-in parser-results [:current-material] new-material)))
+
+(defn- parse-color-line
+  [line color-type parser-results]
+  (let [[_ & args]       (str/split line #"\s+")
+        color            (vec (map #(Double/parseDouble %) args))
+        current-material (:current-material parser-results)]
+    (assoc-in parser-results [:materials current-material color-type] color)))
+
+;; Ambient color statement
+(defmethod parse-line "Ka"
+  [line parser-results]
+  (parse-color-line line :ambient parser-results))
+
+;; Diffuse color statement
+(defmethod parse-line "Kd"
+  [line parser-results]
+  (parse-color-line line :diffuse parser-results))
+
+;; Specular color statement
+(defmethod parse-line "Ks"
+  [line parser-results]
+  (parse-color-line line :specular parser-results))
+
+;; Handle for all blank lines and other non-supported statements
+(defmethod parse-line :default
+  [_ parser-results]
+  parser-results)
+
+(defn parse-file
+  [filename]
+  (with-open [reader (io/reader (io/resource filename))]
+    (let [lines (line-seq reader)]
+      (loop [results (make-empty-parse-results)
+             [current & remaining] lines]
+        (let [new-results (parse-line current results)]
+          (if (empty? remaining)
+            new-results
+            (recur new-results remaining)))))))
---
+++ test/scintilla/mtl_test.clj
@@ -0,0 +1,21 @@
+(ns scintilla.mtl-test
+  (:require [clojure.test :refer :all]
+            [scintilla.numeric :refer [≈]]
+            [scintilla.mtl :refer :all]))
+
+(deftest testing-parse-file
+  (testing "file with multiple materials"
+    (let [parsed-results    (parse-file "materials.mtl")
+          expected-materials  {:FirstMaterial {:ambient [1.0 1.0 1.0]
+                                               :diffuse [0.7 0.6 0.5]
+                                               :specular [0.2 0.2 0.25]}
+                               :SecondMaterial {:ambient [0.5 0.0 1.0]
+                                                :diffuse [0.2 0.2 0.2]
+                                                :specular [0.0 0.1 0.15]}}]
+      (testing "current-material is last material"
+        (is (= :SecondMaterial (:current-material parsed-results))))
+      (testing "all attributes are successfully parsed"
+        (is (= expected-materials (:materials parsed-results))))
+      )
+    )
+)
